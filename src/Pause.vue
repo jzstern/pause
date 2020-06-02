238e4e8d28fa649cdb115bddf0119e73a0a889a4
Michael McKain <michael.mckain94@gmail.com>
2020-06-01 20:56:06 -0600

    updated screen

--- src/Pause.vue
+++ src/Pause.vue
@@ -1,71 +1,86 @@
 <script>
-import DonateButton from "./components/DonateButton.vue"
 import MarqueeText from "vue-marquee-text-component";
 
 export default {
   name: "Pause",
   components: {
-    DonateButton,
-    // DonateButton: () => import("./components/DonateButton.vue"),
     MarqueeText
   },
   computed: {
     isMobile() {
       return false;
-    },
-  },
+    }
+  }
 };
 </script>
 
 <template>
   <div class="pause">
     <div class="content">
-      <h3>Loft is off the air</h3>
-      <p class="copy">
-        We wouldn't exist without the black community. We support and join those
-        protesting systemic racism and injustice across the US and the world.
-        <br />
-      </p>
+      <div class="info-container">
+        <h2>loft is off the air</h2>
+        <p class="copy">
+          The music and culture we surround ourselves with largely comes from or has roots in the black community. We support and join those
+          protesting systemic racism and injustice across the United States and across the world.
+          <br />
+        </p>
 
-      <p class="copy">
-        Below are links to some organizations we've given to – Center for
-        Policing Equity, Equal Justice Initiative, and the ACLU. We encourage
-        you to give if you're able. Peace to all.
-        <br />
-      </p>
-      <div class="link-container">
-        <a class="link" href="https://policingequity.org/donate" target="_blank"
-          >Center for Policing Equity</a
-        >
+        <p class="copy">
+          Below are donation links to some organizations we've given to. We encourage
+          you to give if you're able. Peace to all.
+          <br />
+        </p>
+        <div class="link-container">
+          <a
+            class="link"
+            href="https://policingequity.org/donate"
+            target="_blank"
+          >Center for Policing Equity</a>
 
-        <a
-          class="link"
-          href="https://support.eji.org/give/153413/#!/donation/checkout"
-          target="_blank"
-          >Equal Justice Initiative</a
-        >
+          <a
+            class="link"
+            href="https://support.eji.org/give/153413/#!/donation/checkout"
+            target="_blank"
+          >Equal Justice Initiative</a>
 
-        <!-- <a class="link" href="https://www.cuapb.org/t" target="_blank">CUAPB</a> -->
+          <!-- <a class="link" href="https://www.cuapb.org/t" target="_blank">CUAPB</a> -->
 
-        <a
-          class="link"
-          href="https://action.aclu.org/give/now?redirect=donate"
-          target="_blank"
-          >ACLU</a
-        >
+          <a
+            class="link"
+            href="https://action.aclu.org/give/now?redirect=donate"
+            target="_blank"
+          >ACLU</a>
 
-        <a
-          class="more"
-          target="_blank"
-          href="https://blacklivesmatters.carrd.co/"
-          >More ways to help
-          <img class="link-out" src="./assets/LinkOut.svg" />
-        </a>
+          <a class="more" target="_blank" href="https://blacklivesmatters.carrd.co/">
+            More ways to help
+            <img class="link-out" src="./assets/LinkOut.svg" />
+          </a>
+        </div>
       </div>
-      
-      <DonateButton/>
-    </div>
+      <div class="donate-container" v-if="!isMobile">
+        <h2>donate directly using crypto</h2>
 
+        <div class="donate-total-container">
+          <p class="donate-total">$500</p>
+        </div>
+        <p class="copy">
+          Fellow crypto fam, now is your chance. If you have some money lying around in your wallet, put it to real use in one click. Donations will be split equally amongst the charities to the left.
+          <br />
+        </p>
+        <div class="donate-action">
+          <input
+            class="donate-input"
+            type="number"
+            max="100000"
+            placeholder="$0.00"
+            autocomplete="off"
+            autocorrect="off"
+            spellcheck="true"
+          />
+          <div ref="tipButton" class="donate-button" @click="incrementTip" type="button">donate</div>
+        </div>
+      </div>
+    </div>
 
     <marquee-text v-if="!isMobile" class="marquee" :duration="160">
       BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES
@@ -73,47 +88,119 @@ export default {
       BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES
       MATTER | BLACK LIVES MATTER |
     </marquee-text>
-    <marquee-text
-      v-if="!isMobile"
-      class="marquee marquee-bottom"
-      :duration="260"
-    >
+    <marquee-text v-if="!isMobile" class="marquee marquee-bottom" :duration="260">
       SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |
       SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |
       SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |
       SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |
     </marquee-text>
 
-    <marquee-text v-if="isMobile" class="marquee" :duration="60">
-      BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES MATTER |
-    </marquee-text>
-    <marquee-text v-if="isMobile" class="marquee marquee-bottom" :duration="50">
-      SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |
-    </marquee-text>
+    <marquee-text
+      v-if="isMobile"
+      class="marquee"
+      :duration="60"
+    >BLACK LIVES MATTER | BLACK LIVES MATTER | BLACK LIVES MATTER |</marquee-text>
+    <marquee-text
+      v-if="isMobile"
+      class="marquee marquee-bottom"
+      :duration="50"
+    >SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT | SUPPORT THE MOVEMENT |</marquee-text>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import url("https://use.typekit.net/ijc3kul.css");
+// @import "../styles/global.scss";
+
+@font-face {
+  font-family: "monument";
+  src: url("./assets/MonumentExtended-Regular.otf");
+}
+
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+h2 {
+  font-variant: small-caps;
+}
 
 .pause {
-  z-index: 100000;
   position: fixed;
-  background: rgb(26, 26, 26);
+  z-index: 100000;
+  background: rgb(16, 16, 16);
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   color: white;
+  overflow: hidden;
 }
 
 .content {
   position: absolute;
-  top: 47%;
+  top: 20vh;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: space-around;
+  font-family: "neue-haas-unica";
+  font-weight: 300;
+}
+
+.donate-container {
+  width: 450px;
+  margin: 0 5vw;
+}
+
+.donate-total-container {
+  font-size: 100px;
+  text-align: center;
+  border: 2px solid orange;
+  border-radius: 15px;
+  padding: 15px 0px;
+  margin: 0;
+  height: 175px;
+  font-variant-numeric: tabular-nums;
+}
+
+.donate-total {
+  margin: 0;
+  position: relative;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.donate-action {
+  margin-top: 25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.donate-input {
+  padding-left: 22px;
+  height: 50px;
+  border: 1px solid grey;
+  border-radius: 6px;
+  background: none;
+  font-size: 18px;
+  width: 200px;
+  color: white;
+
+  &::placeholder {
+    color: darkgrey;
+  }
+}
+
+.info-container {
   width: 450px;
-  font-family: "neue-haas-unica";
+  margin: 0 5vw;
 }
 
 .copy {
@@ -128,11 +215,11 @@ export default {
   display: inline-block;
   text-align: center;
   text-decoration: none;
-  color: #ffcb20;
+  color: white;
   margin: auto;
   width: 450px;
   box-sizing: border-box;
-  border: 1px solid #ffcb20;
+  border: 1px solid white;
   border-radius: 4px;
   padding: 10px 0px;
   margin-bottom: 15px;
@@ -140,7 +227,8 @@ export default {
 
   &:hover {
     // width: 440px;
-    background-color: #ffcb204f;
+    color: #ffcb20;
+    border: 1px solid #ffcb20;
     transition: 0.25s;
   }
 }
@@ -165,6 +253,14 @@ export default {
   cursor: pointer;
   text-decoration: none;
   color: white;
+
+  &:hover {
+    // width: 440px;
+    color: #ffcb20;
+    fill: #ffcb20;
+    opacity: 1;
+    transition: 0.25s;
+  }
 }
 
 .link-out {
@@ -174,19 +270,92 @@ export default {
   transform: translateY(-1px);
 }
 
-@media (max-width: 650px) {
+.donate-button {
+  // @extend %non-selectable;
+  // @extend %quick-ease;
+  cursor: pointer;
+  background: #ffcb20;
+  // border: 2px solid rgba(44, 25, 92, 0.65);
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: auto 30px;
+  color: rgb(0, 0, 0);
+  font-size: 22px;
+  line-height: 14px;
+  text-align: center;
+
+  // font-variant: small-caps;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  overflow: hidden;
+  position: relative;
+
+  transition: all 0.2s ease;
+  outline: none;
+  box-shadow: 0px 4px 8px rgba(70, 38, 11, 0.35);
+
+  &:active {
+    transform: scale(0.99);
+    background: #ffc400;
+    transition: all 0.1s ease;
+    transform: translateY(2px);
+    box-shadow: 0px 0px rgba(70, 38, 11, 0.25);
+  }
+
+  &:after {
+    background: #ffda5f;
+    content: "";
+    height: 155px;
+    left: -75px;
+    opacity: 0.2;
+    position: absolute;
+    top: -50px;
+    transform: rotate(35deg);
+    transition: all 850ms cubic-bezier(0.19, 1, 0.22, 1);
+    width: 50px;
+    z-index: -10;
+  }
+}
+
+@media (max-width: 1100px) {
+  .donate-container {
+    margin: 0 2vw;
+  }
+
+  .info-container {
+    margin: 0 2vw;
+  }
+}
+
+@media (max-width: 900px) {
   .content {
     top: 50%;
     width: 85vw;
+    transform: translate(-50%, -50%);
+    margin: auto;
   }
+
   .copy {
     text-align: justify;
     font-size: 15px;
   }
 
+  .donate-container {
+    display: none;
+  }
+
+  .info-container {
+    margin: auto;
+  }
+
   .link {
     width: 85vw;
+    max-width: 450px;
     padding: 7px 0px;
+    color: #ffcb20;
+    border: 1px solid #ffcb20;
   }
   .marquee {
     font-size: 22px;
